3c35f60e3aa378b0af6d44907063285923f9e853
Jack Road <462752802@qq.com>
2020-03-17 14:56:38 +0800

    update--cloud-provide-payment8001-module

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,41 @@
+package com.atguigu.springcloud.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    PaymentService paymentService;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(@RequestBody Payment payment) {
+        int result = paymentService.create(payment);
+        log.info("插入结果:" + result);
+        if (result > 0) {
+            return new CommonResult(200, "插入成功", result);
+        } else {
+            return new CommonResult(444, "插入失败", null);
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+        Payment paymentById = paymentService.getPaymentById(id);
+        log.info("查询结果:" + paymentById);
+        if (paymentById != null) {
+            return new CommonResult(200, "查询成功", paymentById);
+        } else {
+            return new CommonResult(444, "没有对应记录，查询id:" + id, null);
+        }
+    }
+}
