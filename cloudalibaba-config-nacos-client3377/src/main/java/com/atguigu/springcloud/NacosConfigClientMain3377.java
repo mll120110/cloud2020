c32a1a26c24ca593572ef91df7a365e7ff88eedd
Jack Road <462752802@qq.com>
2020-04-24 18:32:04 +0800

    update--nacos config

--- cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/NacosConfigClientMain3377.java
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/NacosConfigClientMain3377.java
@@ -1,11 +1,16 @@
 package com.atguigu.springcloud;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+/**
+ * mybatis-plus时注入mapper失败的问题
+ */
+@MapperScan(value = "com.atguigu")
 public class NacosConfigClientMain3377 {
     public static void main(String[] args) {
         SpringApplication.run(NacosConfigClientMain3377.class, args);
